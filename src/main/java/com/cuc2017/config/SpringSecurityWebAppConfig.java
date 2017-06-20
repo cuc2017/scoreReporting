277390bea4eb2f8d760a841c2b180760baf2528c
Rob Tyson <rob@cuc2017.com>
2017-06-20 09:47:56 -0400

    Initial commit

---
+++ src/main/java/com/cuc2017/config/SpringSecurityWebAppConfig.java
@@ -0,0 +1,34 @@
+package com.cuc2017.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.stormpath.sdk.authc.AuthenticationResult;
+import com.stormpath.sdk.client.Client;
+import com.stormpath.sdk.servlet.http.Saver;
+
+@Configuration
+public class SpringSecurityWebAppConfig extends WebSecurityConfigurerAdapter {
+
+	@Value("#{ @environment['stormpath.web.login.nextUri'] ?: '/' }")
+	protected String loginNextUri;
+
+	@Value("#{ @environment['stormpath.web.produces'] ?: 'application/json, text/html' }")
+	protected String produces;
+
+	@Autowired
+	@Qualifier("stormpathAuthenticationResultSaver")
+	protected Saver<AuthenticationResult> authenticationResultSaver;
+
+	@Autowired
+	protected Client client;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/restricted").fullyAuthenticated().antMatchers("/**").permitAll();
+	}
+}
---
+++ src/main/java/com/cuc2017/controller/HelloController.java
@@ -0,0 +1,34 @@
+package com.cuc2017.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cuc2017.service.HelloService;
+import com.stormpath.sdk.servlet.account.AccountResolver;
+
+@Controller
+public class HelloController {
+
+	@Autowired
+	private HelloService helloService;
+
+	@Autowired
+	AccountResolver accountResolver;
+
+	@RequestMapping("/")
+	String home(HttpServletRequest request) {
+		return "home";
+	}
+
+	@RequestMapping("/restricted")
+	String restricted(HttpServletRequest request, Model model) {
+		String msg = helloService.sayHello(accountResolver.getAccount(request));
+		model.addAttribute("msg", msg);
+		return "restricted";
+	}
+
+}
