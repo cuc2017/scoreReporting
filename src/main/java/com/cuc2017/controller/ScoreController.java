0d076f084570c985bdfdf1c37a5fefdbc5cc9d75
Rob Tyson <rob@ryomic.com>
2017-06-26 09:33:51 -0400

    Fix warnings

--- src/main/java/com/cuc2017/controller/ScoreController.java
+++ src/main/java/com/cuc2017/controller/ScoreController.java
@@ -2,8 +2,6 @@ package com.cuc2017.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,7 +12,8 @@ import com.cuc2017.repository.FieldRepository;
 @Controller
 public class ScoreController {
 
-	private static final Logger log = LoggerFactory.getLogger(ScoreController.class);
+	// private static final Logger log =
+	// LoggerFactory.getLogger(ScoreController.class);
 
 	private FieldRepository fieldRepository;
 
